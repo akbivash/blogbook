1a5eb57c8c396f4805079c664c94fa572ca96d15
akbiv <akbivash@gmail.com>
2023-02-22 18:59:14 +0545

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import Navbar from '@/components/Navbar'
 import {FaGraduationCap} from 'react-icons/fa'
 import Link from 'next/link'
 import Homepage from '@/components/Homepage'
 import Footer from '@/components/Footer'
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
