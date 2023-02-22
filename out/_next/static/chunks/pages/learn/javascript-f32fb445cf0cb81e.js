d070f14a1baca6abdd147a1abebde061ac8e7c3f
akbiv <akbivash@gmail.com>
2023-02-22 19:30:14 +0545

    changes

---
+++ out/_next/static/chunks/pages/learn/javascript-f32fb445cf0cb81e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[807],{2400:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/learn/javascript",function(){return e(4034)}])},4034:function(n,u,e){"use strict";e.r(u);var i=e(5893);e(7294);let t=()=>(0,i.jsx)("div",{children:"index"});u.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2400)}),_N_E=n.O()}]);
