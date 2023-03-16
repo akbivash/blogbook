d9264b471448f709a7c9b2f89ecf2a27484f41fe
akbiv <akbivash@gmail.com>
2023-03-16 08:50:15 +0545

    l

--- pages/user/signup.tsx
+++ pages/user/signup.tsx
@@ -25,7 +25,7 @@ const{signUp, errorMsg, isLoading, signInWithGoogle} = useContext(AuthContext)
       .max(40, "Password must not exceed 40 characters"),
     confirmPassword: Yup.string()
       .required("Confirm Password is required")
-      .oneOf([Yup.ref("password"), null], "Confirm Password does not match"),
+      .oneOf([Yup.ref("password")], "Confirm Password does not match"),
   });
 
   const {
